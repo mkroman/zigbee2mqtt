e8d81b894d45512d4b311dba794690393ce42351
Mikkel Kroman <mk@maero.dk>
2019-01-27 07:40:36 -0800

    Add platform for build command and enable experimental docker

--- .travis/docker.sh
+++ .travis/docker.sh
@@ -10,7 +10,9 @@ tag_push() {
 }
 
 build_and_push() {
-  docker build --build-arg COMMIT=$(git rev-parse --short HEAD) -t $DOCKER_USERNAME/zigbee2mqtt:$1 -f $2 .
+  platform=${3:-amd64}
+
+  docker build --platform=$platform --build-arg COMMIT=$(git rev-parse --short HEAD) -t $DOCKER_USERNAME/zigbee2mqtt:$1 -f $2 .
   docker push $DOCKER_USERNAME/zigbee2mqtt:$1
 }
 
@@ -38,6 +40,14 @@ then
   build_and_push latest-dev docker/Dockerfile.amd64
   build_and_push arm32v6-dev docker/Dockerfile.arm32v6
   build_and_push arm64v8-dev docker/Dockerfile.arm64v8
+elif [ "$TRAVIS_BRANCH" = "fix-arm-docker-images" -a "$TRAVIS_PULL_REQUEST" = "false" ]
+then
+  # FIXME: remove this before merge
+  echo "Updating docker images for dev branch!"
+  login
+  build_and_push latest-dev docker/Dockerfile.amd64
+  build_and_push arm32v6-dev docker/Dockerfile.arm32v6 arm
+  build_and_push arm64v8-dev docker/Dockerfile.arm64v8 arm64
 else
   echo "Not updating docker images, triggered by pull request or not on master/dev branch"
 fi
